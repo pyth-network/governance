a9effbe6e56b52700ce9d63a657976a8a641e2ee
Daniel Chew <cctdaniel@outlook.com>
2022-03-08 10:50:50 +0800

    add SEO (#12)

---
+++ frontend/components/config.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'Pyth Network',
+  author: {
+    name: 'Pyth Network',
+  },
+  description:
+    'Pyth is building a way to deliver a decentralized, cross-chain market of verifiable data from high-quality nodes to any smart contract, anywhere.',
+}
