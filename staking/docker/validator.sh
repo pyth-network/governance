09df29369e168a08ba3f7c0f124735164e8234a8
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-08 00:36:41 -0500

    validator commited

---
+++ staking/docker/validator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export STAKING_PROGRAM="Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"
+export PUBKEY=$(solana-keygen pubkey ./tests/default_wallet.json)
+
+solana-test-validator --ledger ./.anchor/test-ledger --mint ${PUBKEY} --reset --bpf-program  ${STAKING_PROGRAM} ./target/deploy/staking.so
