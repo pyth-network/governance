3c36cf434e94b16ea44504aaaf1df4dc6c1ea2fe
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-09 00:23:01 -0500

    beginning to build some api functionalities in app/api.ts

--- staking/docker/validator.sh
+++ staking/docker/validator.sh
@@ -2,4 +2,6 @@
 export STAKING_PROGRAM="Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"
 export PUBKEY=$(solana-keygen pubkey ./tests/default_wallet.json)
 
-solana-test-validator --ledger ./.anchor/test-ledger --mint ${PUBKEY} --reset --bpf-program  ${STAKING_PROGRAM} ./target/deploy/staking.so
+solana-test-validator --ledger ./.anchor/test-ledger --mint ${PUBKEY} --reset --bpf-program  ${STAKING_PROGRAM} ./target/deploy/staking.so &
+sleep 3
+anchor idl init --filepath target/idl/staking.json Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS
