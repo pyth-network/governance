956280d5aeab5c98cba76b96170e625739a3f126
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-10 00:02:13 -0500

    api ideas

--- staking-ts/src/StakeConnection.ts
+++ staking-ts/src/StakeConnection.ts
@@ -1,5 +1,5 @@
-import { Provider, Program, Wallet} from "@project-serum/anchor";
-import { Keypair, PublicKey, Connection } from "@solana/web3.js";
+import { Provider, Program} from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
 
 export class StakeConnection {
     provider : Provider
@@ -12,24 +12,42 @@ export class StakeConnection {
         this.owner = provider.wallet.publicKey 
     }
 
-    //finds the address of this.owner stake accounts
-    public async findStakeAccounts() : Promise<PublicKey[]> {
-        
+    //gets this.owner stake accounts
+    public async getStakeAccounts() : Promise<StakeAccount[]> {
         return 
     }
 
-    //gets all the (neatly structured) data of a user's stake account using the address from the above function
-    public async loadStakeAccount( stake_account_address : PublicKey){
+    public async createStakeAccount() : Promise<StakeAccount> {
+        return
     }
 
-    //lock a provided token balance
-    public async lockTokens(amount : number){
+}
 
+
+export class StakeAccount {
+    provider : Provider
+    stake_program : Program
+    owner : PublicKey
+    address : PublicKey
+    
+
+    constructor(provider : Provider, stake_program : Program, address : PublicKey){
+        this.address = address;
+        this.provider = provider;
+        this.stake_program = stake_program;
     }
+
+    async load(){
+    }
+
 
     //unlock a provided token balance
     public async unlockTokens(amount : number){
 
+    }
+
+    //deposit tokens
+    public async depositAndLockTokens(amount : number){
     }
 
     //withdraw tokens
@@ -37,27 +55,24 @@ export class StakeConnection {
 
     }
 
-    public async getUnlockingSchedule(){
-        // What is the best way to represent current unlocking schedule in the UI
+    // Withdrawable
+    
+    //Locked tokens :
+     // - warmup 
+     // - active 
+     // - cooldown
+
+    // Unvested
+
+    public getBalanceSummary(){
+        
     }
 
-    public async getVestingSchedule(){
-        // What is the best way to represent current vesting schedule in the UI
-    }
-}
 
-export class StakeAccount {
-    address : PublicKey
-    withdrawable_balance : number
-    locked_balance : number
-    unvested_balance : number
-    data
-
-    constructor(address : PublicKey){
-        this.address = address;
+     // What is the best way to represent current vesting schedule in the UI
+    public getVestingSchedule(){
+       
     }
 
-    async load(){
-        // populate all other fields
-    }
+
 }
