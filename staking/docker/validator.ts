540d9f7d7491905c32e4c701a3c7bd49dcab8431
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-10 16:30:55 -0500

    everything gets read from anchor.toml

--- staking/docker/validator.ts
+++ staking/docker/validator.ts
@@ -6,21 +6,20 @@ import { exec } from "child_process";
 
 const config = toml.parse(fs.readFileSync("./Anchor.toml").toString());
 
-const testing_dir = "./.anchor/test-ledger"
+const ledger_dir = config.validator.ledger_dir
 const wallet_pubkey_path = config.provider.wallet;
-
 const program_address = new PublicKey(config.programs.localnet.staking);
 
 exec(`solana-keygen new -o ${wallet_pubkey_path} --no-bip39-passphrase --force`)
 
 const wallet_pubkey = Keypair.fromSecretKey(
     new Uint8Array(
-      JSON.parse(fs.readFileSync("../staking/app/keypairs/alice.json").toString())
+      JSON.parse(fs.readFileSync(wallet_pubkey_path).toString())
     )
   ).publicKey
 
-exec(`mkdir -p ${testing_dir}`)
-exec(`solana-test-validator --ledger ${testing_dir} --mint ${wallet_pubkey} --reset --bpf-program  ${program_address} ./target/deploy/staking.so &`)
+exec(`mkdir -p ${ledger_dir}`)
+exec(`solana-test-validator --ledger ${ledger_dir} --mint ${wallet_pubkey} --reset --bpf-program  ${program_address} ./target/deploy/staking.so &`)
 exec("sleep 3");
 exec(`anchor idl init --filepath target/idl/staking.json ${program_address}`)
 exec("fg")
