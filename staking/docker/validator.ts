d8f824e0754973c0e0386dfa724eca5e70796a03
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-10 23:36:38 -0500

    synchrnous execution

--- staking/docker/validator.ts
+++ staking/docker/validator.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import toml from 'toml';
 import {PublicKey, Keypair } from "@solana/web3.js";
 import { exec } from "child_process";
+import shell from "shelljs"
 
 const config = toml.parse(fs.readFileSync("./Anchor.toml").toString());
 
@@ -9,10 +10,9 @@ const ledger_dir = config.validator.ledger_dir
 const wallet_pubkey_path = config.provider.wallet;
 const program_address = new PublicKey(config.programs.localnet.staking);
 
-exec("set -m")
-exec(`solana-keygen new -o ${wallet_pubkey_path} --no-bip39-passphrase --force`)
 
-exec("anchor build")
+shell.exec(`solana-keygen new -o ${wallet_pubkey_path} --no-bip39-passphrase --force`)
+shell.exec("anchor build")
 
 const wallet_pubkey = Keypair.fromSecretKey(
     new Uint8Array(
@@ -20,7 +20,7 @@ const wallet_pubkey = Keypair.fromSecretKey(
     )
   ).publicKey
 
-exec(`mkdir -p ${ledger_dir}`)
+shell.exec(`mkdir -p ${ledger_dir}`)
 exec(`solana-test-validator --ledger ${ledger_dir} --mint ${wallet_pubkey} --reset --bpf-program  ${program_address} ./target/deploy/staking.so`)
-exec("sleep 3");
-exec(`anchor idl init --filepath target/idl/staking.json ${program_address}`)
+
+shell.exec(`sleep 3 && anchor idl init --filepath target/idl/staking.json ${program_address}`)
