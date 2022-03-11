6a72553fe24df9b165acead8f5c276979ddcc5e0
Philip Taffet <ptaffet@rice.edu>
2022-03-11 17:01:50 -0600

    Extra defense against deploying mock-clock in prod

--- staking/programs/staking/src/utils/clock.rs
+++ staking/programs/staking/src/utils/clock.rs
@@ -8,11 +8,22 @@ pub fn get_current_epoch(global_config : &GlobalConfig) -> Result<u64>{
     return now_ts.checked_div(global_config.epoch_duration).ok_or(error!(ErrorCode::ZeroEpochDuration));
 }
 
+// As an extra form of defense to make sure we're not using the mock clock
+// in devnet or mainnet, we'd like to have an assert(localnet). There's not 
+// an easy way to do that, but something that gets close is checking that the
+// number of slots that have passed is much smaller than anything possible on
+// mainnet or devnet. We set the threshold at 10 million slots, which is more 
+// than a month. mainnet, devnet, and testnet are all > 140 million right now.
+#[cfg(feature = "mock-clock")]
+const MAX_LOCALNET_VALIDATOR_RUNTIME_SLOTS : u64 = 10_000_000;
+
 #[cfg(feature = "mock-clock")]
 pub fn get_current_time(global_config : &GlobalConfig) -> UnixTimestamp {
+
+    assert!(Clock::get().unwrap().slot < MAX_LOCALNET_VALIDATOR_RUNTIME_SLOTS);
     global_config.mock_clock_time
 }
 #[cfg(not(feature = "mock-clock"))]
-    pub fn get_current_time(global_config : &GlobalConfig) -> UnixTimestamp {
+    pub fn get_current_time(_global_config : &GlobalConfig) -> UnixTimestamp {
     Clock::get().unwrap().unix_timestamp
 } 
