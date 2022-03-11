34d3f9aa545ab6b772849c542bd47404439d4fda
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-11 14:57:18 -0500

    replace sleep by loop until validator responds

--- staking/docker/validator.ts
+++ staking/docker/validator.ts
@@ -1,26 +1,46 @@
 import fs from "fs";
-import toml from 'toml';
-import {PublicKey, Keypair } from "@solana/web3.js";
+import toml from "toml";
+import { PublicKey, Keypair, Connection } from "@solana/web3.js";
 import { exec } from "child_process";
-import shell from "shelljs"
+import shell from "shelljs";
+import { getMultipleAccounts } from "@project-serum/anchor/dist/cjs/utils/rpc";
 
-const config = toml.parse(fs.readFileSync("./Anchor.toml").toString());
+async function main() {
+  const config = toml.parse(fs.readFileSync("./Anchor.toml").toString());
 
-const ledger_dir = config.validator.ledger_dir
-const wallet_pubkey_path = config.provider.wallet;
-const program_address = new PublicKey(config.programs.localnet.staking);
+  const ledger_dir = config.validator.ledger_dir;
+  const wallet_pubkey_path = config.provider.wallet;
+  const program_address = new PublicKey(config.programs.localnet.staking);
 
+  shell.exec(
+    `solana-keygen new -o ${wallet_pubkey_path} --no-bip39-passphrase --force`
+  );
+  shell.exec("anchor build");
 
-shell.exec(`solana-keygen new -o ${wallet_pubkey_path} --no-bip39-passphrase --force`)
-shell.exec("anchor build")
+  const wallet_pubkey = Keypair.fromSecretKey(
+    new Uint8Array(JSON.parse(fs.readFileSync(wallet_pubkey_path).toString()))
+  ).publicKey;
 
-const wallet_pubkey = Keypair.fromSecretKey(
-    new Uint8Array(
-      JSON.parse(fs.readFileSync(wallet_pubkey_path).toString())
-    )
-  ).publicKey
+  shell.exec(`mkdir -p ${ledger_dir}`);
 
-shell.exec(`mkdir -p ${ledger_dir}`)
-exec(`solana-test-validator --ledger ${ledger_dir} --mint ${wallet_pubkey} --reset --bpf-program  ${program_address} ./target/deploy/staking.so`)
-// we sleep 3 because the validator running in the background needs to get started before we upload the blockchain idl
-shell.exec(`sleep 3 && anchor idl init --filepath target/idl/staking.json ${program_address}`)
+  exec(
+    `solana-test-validator --ledger ${ledger_dir} --mint ${wallet_pubkey} --reset --bpf-program  ${program_address} ./target/deploy/staking.so`
+  );
+
+  //wait until validator is responsive
+  const connection = new Connection("http://localhost:8899");
+  while (true) {
+    try {
+      console.log("waiting for validator");
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await connection.getEpochInfo();
+      break;
+    } catch (e) {}
+  }
+
+  shell.exec(
+    `anchor idl init --filepath target/idl/staking.json ${program_address}`
+  );
+}
+
+main();
