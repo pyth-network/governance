6eebcaaa13eb12f1d569f783b4f4d6dca77542cb
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-14 14:56:50 -0500

    action for api?

--- staking-ts/tests/test.ts
+++ staking-ts/tests/test.ts
@@ -1,44 +1,27 @@
 import { Keypair, PublicKey, Connection } from "@solana/web3.js";
-import { Provider, Program, Wallet } from "@project-serum/anchor";
+import { Provider, Wallet } from "@project-serum/anchor";
 import fs from "fs";
-import { StakeConnection, StakeAccount} from "../src"
+import { StakeConnection} from "../src"
 
+// let's try to get rid of this magic constant
 const staking_program = new PublicKey(
     "Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS"
   );
 
-async function main() {
+describe("api", async () => {
+
+  it("parses accounts", async () => {
 
     const alice = Keypair.fromSecretKey(
-    new Uint8Array(
-      JSON.parse(fs.readFileSync("../staking/app/keypairs/alice.json").toString())
-    )
-  );
-
-  const connection: Connection = new Connection("http://localhost:8899");
-
-  const provider = new Provider(connection, new Wallet(alice), {
-    preflightCommitment: "recent",
-  });
-
-
-  const stake_connection : StakeConnection = await StakeConnection.createStakeConnection(connection, new Wallet(alice), staking_program);
-
-  const res = await stake_connection.getStakeAccounts(alice.publicKey);
-
-
-
-  console.log(res);
-
-
-
-//   console.log(stake_account_connection.config);
-
+      new Uint8Array(
+        JSON.parse(fs.readFileSync("../staking/app/keypairs/alice.json").toString())
+      )
+    );
   
-//   const new_stake = await stake_account_connection.createStakeAccount();
-//   console.log(new_stake);
-
-}
-
-main();
-//   const program = new anchor.Program(idl, CANDY_MACHINE_PROGRAM_ID, provider);
+    const connection: Connection = new Connection("http://localhost:8899");
+  
+    const stake_connection : StakeConnection = await StakeConnection.createStakeConnection(connection, new Wallet(alice), staking_program);
+  
+    const res = await stake_connection.getStakeAccounts(alice.publicKey);
+  });
+});
