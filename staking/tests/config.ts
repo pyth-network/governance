eba600fa63265baf331627b787fdc2632db81f81
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-17 14:39:55 -0500

    each testfile spawns own validator

---
+++ staking/tests/config.ts
@@ -0,0 +1,179 @@
+import { Program, Provider, Wallet, utils } from "@project-serum/anchor";
+import { PublicKey, Keypair, Transaction, Connection } from "@solana/web3.js";
+import { createMint } from "./utils/utils";
+import BN from "bn.js";
+import assert from "assert";
+import fs from "fs";
+import toml from "toml";
+import { exec } from "child_process";
+import {
+  TOKEN_PROGRAM_ID,
+  Token,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+
+// When DEBUG is turned on, we turn preflight transaction checking off
+// That way failed transactions show up in the explorer, which makes them
+// easier to debug.
+const DEBUG = true;
+
+describe("config", async () => {
+  const portNumber = 8899;
+
+  const CONFIG_SEED = "config";
+
+  const pythMintAccount = new Keypair();
+  const pythMintAuthority = new Keypair();
+  const zeroPubkey = new PublicKey(0);
+
+  const config = toml.parse(fs.readFileSync("./Anchor.toml").toString());
+
+  const user = Keypair.fromSecretKey(
+    new Uint8Array(
+      JSON.parse(fs.readFileSync(config.provider.wallet).toString())
+    )
+  );
+  const ledgerDir = config.validator.ledger_dir;
+  const programAddress = new PublicKey(config.programs.localnet.staking);
+  const idlPath = config.build.idl_path;
+  const binaryPath = config.build.binary_path;
+
+  const connection: Connection = new Connection(
+    `http://localhost:${portNumber}`,
+    Provider.defaultOptions().commitment
+  );
+
+  let provider;
+  let program;
+  const controller = new AbortController();
+  const { signal } = controller;
+
+  let configAccount: PublicKey;
+  let bump: number;
+
+  [configAccount, bump] = await PublicKey.findProgramAddress(
+    [utils.bytes.utf8.encode(CONFIG_SEED)],
+    programAddress
+  );
+
+  after(async () => {
+    controller.abort();
+  });
+
+  it("deploy program", async () => {
+    exec(
+      `mkdir -p ${ledgerDir}/${portNumber} && solana-test-validator --ledger ${ledgerDir}/${portNumber} --rpc-port ${portNumber} --mint ${
+        user.publicKey
+      } --reset --bpf-program  ${programAddress.toBase58()} ${binaryPath}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      }
+    );
+
+    while (true) {
+      try {
+        console.log("waiting");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await connection.getEpochInfo();
+        break;
+      } catch (e) {}
+    }
+
+    provider = new Provider(connection, new Wallet(user), {});
+    program = new Program(
+      JSON.parse(fs.readFileSync(idlPath).toString()),
+      programAddress,
+      provider
+    );
+  });
+
+  it("creates mint", async () => {
+    await createMint(
+      provider,
+      pythMintAccount,
+      pythMintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+  });
+
+
+  it("initializes config", async () => {
+    await program.methods
+      .initConfig({
+        governanceAuthority: provider.wallet.publicKey,
+        pythTokenMint: pythMintAccount.publicKey,
+        unlockingDuration: 2,
+        epochDuration: new BN(3600),
+        mockClockTime: new BN(10),
+      })
+      .rpc({
+        skipPreflight: DEBUG,
+      });
+
+    const configAccountData = await program.account.globalConfig.fetch(
+      configAccount
+    );
+
+    assert.equal(
+      JSON.stringify(configAccountData),
+      JSON.stringify({
+        bump,
+        governanceAuthority: provider.wallet.publicKey,
+        pythTokenMint: pythMintAccount.publicKey,
+        pythGovernanceRealm: zeroPubkey,
+        unlockingDuration: 2,
+        epochDuration: new BN(3600),
+        mockClockTime: new BN(10),
+      })
+    );
+  });
+
+  it("advances clock twice", async () => {
+    await program.methods.advanceClock(new BN(5)).rpc({ skipPreflight: DEBUG });
+
+    let configAccountData = await program.account.globalConfig.fetch(
+      configAccount
+    );
+
+    assert.equal(
+      JSON.stringify(configAccountData),
+      JSON.stringify({
+        bump,
+        governanceAuthority: provider.wallet.publicKey,
+        pythTokenMint: pythMintAccount.publicKey,
+        pythGovernanceRealm: zeroPubkey,
+        unlockingDuration: 2,
+        epochDuration: new BN(3600),
+        mockClockTime: new BN(15),
+      })
+    );
+
+    await program.methods
+      .advanceClock(new BN(15))
+      .rpc({ skipPreflight: DEBUG });
+
+    configAccountData = await program.account.globalConfig.fetch(
+      configAccount
+    );
+
+    assert.equal(
+      JSON.stringify(configAccountData),
+      JSON.stringify({
+        bump,
+        governanceAuthority: provider.wallet.publicKey,
+        pythTokenMint: pythMintAccount.publicKey,
+        pythGovernanceRealm: zeroPubkey,
+        unlockingDuration: 2,
+        epochDuration: new BN(3600),
+        mockClockTime: new BN(30),
+      })
+    );
+  });
+});
--- staking/tests/create_account.ts
+++ staking/tests/create_account.ts
@@ -1,181 +1,181 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Staking } from "../target/types/staking";
-import { depositTokensInstruction, createStakeAccount } from "./utils/utils";
+// import * as anchor from "@project-serum/anchor";
+// import { Program } from "@project-serum/anchor";
+// import { Staking } from "../target/types/staking";
+// import { depositTokensInstruction, createStakeAccount } from "./utils/utils";
 
-import {
-  TOKEN_PROGRAM_ID,
-  Token,
-  ASSOCIATED_TOKEN_PROGRAM_ID,
-} from "@solana/spl-token";
+// import {
+//   TOKEN_PROGRAM_ID,
+//   Token,
+//   ASSOCIATED_TOKEN_PROGRAM_ID,
+// } from "@solana/spl-token";
 
-import {
-  PublicKey,
-  Keypair,
-  Transaction,
-  SystemProgram,
-} from "@solana/web3.js";
+// import {
+//   PublicKey,
+//   Keypair,
+//   Transaction,
+//   SystemProgram,
+// } from "@solana/web3.js";
 
-import assert from "assert";
-import fs from "fs";
+// import assert from "assert";
+// import fs from "fs";
 
-const DEBUG = true;
+// const DEBUG = true;
 
-describe("create_stake_account", async () => {
-  let program: Program<Staking>;
+// describe("create_stake_account", async () => {
+//   let program: Program<Staking>;
 
-  let config_account: PublicKey;
-  let bump: number;
+//   let config_account: PublicKey;
+//   let bump: number;
 
-  let errMap: Map<number, string>;
+//   let errMap: Map<number, string>;
 
-  const CONFIG_SEED = "config";
-  const STAKE_ACCOUNT_METADATA_SEED = "stake_metadata";
-  const CUSTODY_SEED = "custody";
-  const AUTHORITY_SEED = "authority";
-  const VOTER_SEED = "voter_weight";
+//   const CONFIG_SEED = "config";
+//   const STAKE_ACCOUNT_METADATA_SEED = "stake_metadata";
+//   const CUSTODY_SEED = "custody";
+//   const AUTHORITY_SEED = "authority";
+//   const VOTER_SEED = "voter_weight";
 
-  const stake_account_positions_secret = new Keypair();
+//   const stake_account_positions_secret = new Keypair();
 
-  let pyth_mint_account;
-  let pyth_mint_authority;
-  const zero_pubkey = new PublicKey(0);
+//   let pyth_mint_account;
+//   let pyth_mint_authority;
+//   const zero_pubkey = new PublicKey(0);
 
-  let user_ata;
+//   let user_ata;
 
-  before(async () => {
-    program = anchor.workspace.Staking as Program<Staking>;
+//   before(async () => {
+//     program = anchor.workspace.Staking as Program<Staking>;
 
-    [config_account, bump] = await PublicKey.findProgramAddress(
-      [anchor.utils.bytes.utf8.encode(CONFIG_SEED)],
-      program.programId
-    );
+//     [config_account, bump] = await PublicKey.findProgramAddress(
+//       [anchor.utils.bytes.utf8.encode(CONFIG_SEED)],
+//       program.programId
+//     );
 
-    while (true) {
-      try {
-        console.log("waiting");
-        await new Promise((resolve) => setTimeout(resolve, 1000));
-        await program.account.globalConfig.fetch(config_account);
-        break;
-      } catch (e) {}
-    }
+//     while (true) {
+//       try {
+//         console.log("waiting");
+//         await new Promise((resolve) => setTimeout(resolve, 1000));
+//         await program.account.globalConfig.fetch(config_account);
+//         break;
+//       } catch (e) {}
+//     }
 
-    pyth_mint_account = Keypair.fromSecretKey(
-      new Uint8Array(
-        JSON.parse(fs.readFileSync("./tests/pyth_mint_account.json").toString())
-      )
-    );
+//     pyth_mint_account = Keypair.fromSecretKey(
+//       new Uint8Array(
+//         JSON.parse(fs.readFileSync("./tests/pyth_mint_account.json").toString())
+//       )
+//     );
 
-    pyth_mint_authority = Keypair.fromSecretKey(
-      new Uint8Array(
-        JSON.parse(
-          fs.readFileSync("./tests/pyth_mint_authority.json").toString()
-        )
-      )
-    );
+//     pyth_mint_authority = Keypair.fromSecretKey(
+//       new Uint8Array(
+//         JSON.parse(
+//           fs.readFileSync("./tests/pyth_mint_authority.json").toString()
+//         )
+//       )
+//     );
 
-    user_ata = await Token.getAssociatedTokenAddress(
-      ASSOCIATED_TOKEN_PROGRAM_ID,
-      TOKEN_PROGRAM_ID,
-      pyth_mint_account.publicKey,
-      program.provider.wallet.publicKey
-    );
+//     user_ata = await Token.getAssociatedTokenAddress(
+//       ASSOCIATED_TOKEN_PROGRAM_ID,
+//       TOKEN_PROGRAM_ID,
+//       pyth_mint_account.publicKey,
+//       program.provider.wallet.publicKey
+//     );
 
-    errMap = anchor.parseIdlErrors(program.idl);
-  });
+//     errMap = anchor.parseIdlErrors(program.idl);
+//   });
 
-  it("creates vested staking account", async () => {
-    const owner = program.provider.wallet.publicKey;
+//   it("creates vested staking account", async () => {
+//     const owner = program.provider.wallet.publicKey;
 
-    const [metadataAccount, metadataBump] = await PublicKey.findProgramAddress(
-      [
-        anchor.utils.bytes.utf8.encode(STAKE_ACCOUNT_METADATA_SEED),
-        stake_account_positions_secret.publicKey.toBuffer(),
-      ],
-      program.programId
-    );
+//     const [metadataAccount, metadataBump] = await PublicKey.findProgramAddress(
+//       [
+//         anchor.utils.bytes.utf8.encode(STAKE_ACCOUNT_METADATA_SEED),
+//         stake_account_positions_secret.publicKey.toBuffer(),
+//       ],
+//       program.programId
+//     );
 
-    const [custodyAccount, custodyBump] = await PublicKey.findProgramAddress(
-      [
-        anchor.utils.bytes.utf8.encode(CUSTODY_SEED),
-        stake_account_positions_secret.publicKey.toBuffer(),
-      ],
-      program.programId
-    );
+//     const [custodyAccount, custodyBump] = await PublicKey.findProgramAddress(
+//       [
+//         anchor.utils.bytes.utf8.encode(CUSTODY_SEED),
+//         stake_account_positions_secret.publicKey.toBuffer(),
+//       ],
+//       program.programId
+//     );
 
-    const [authorityAccount, authorityBump] =
-      await PublicKey.findProgramAddress(
-        [
-          anchor.utils.bytes.utf8.encode(AUTHORITY_SEED),
-          stake_account_positions_secret.publicKey.toBuffer(),
-        ],
-        program.programId
-      );
+//     const [authorityAccount, authorityBump] =
+//       await PublicKey.findProgramAddress(
+//         [
+//           anchor.utils.bytes.utf8.encode(AUTHORITY_SEED),
+//           stake_account_positions_secret.publicKey.toBuffer(),
+//         ],
+//         program.programId
+//       );
 
-    const [voterAccount, voterBump] = await PublicKey.findProgramAddress(
-      [
-        anchor.utils.bytes.utf8.encode(VOTER_SEED),
-        stake_account_positions_secret.publicKey.toBuffer(),
-      ],
-      program.programId
-    );
+//     const [voterAccount, voterBump] = await PublicKey.findProgramAddress(
+//       [
+//         anchor.utils.bytes.utf8.encode(VOTER_SEED),
+//         stake_account_positions_secret.publicKey.toBuffer(),
+//       ],
+//       program.programId
+//     );
 
-    await createStakeAccount(
-      program,
-      stake_account_positions_secret,
-      pyth_mint_account.publicKey
-    );
+//     await createStakeAccount(
+//       program,
+//       stake_account_positions_secret,
+//       pyth_mint_account.publicKey
+//     );
 
-    const stake_account_metadata_data =
-      await program.account.stakeAccountMetadata.fetch(metadataAccount);
+//     const stake_account_metadata_data =
+//       await program.account.stakeAccountMetadata.fetch(metadataAccount);
 
-    assert.equal(
-      JSON.stringify(stake_account_metadata_data),
-      JSON.stringify({
-        metadataBump,
-        custodyBump,
-        authorityBump,
-        voterBump,
-        owner,
-        lock: { fullyVested: {} },
-      })
-    );
-  });
+//     assert.equal(
+//       JSON.stringify(stake_account_metadata_data),
+//       JSON.stringify({
+//         metadataBump,
+//         custodyBump,
+//         authorityBump,
+//         voterBump,
+//         owner,
+//         lock: { fullyVested: {} },
+//       })
+//     );
+//   });
 
-  it("deposits tokens", async () => {
-    const transaction = new Transaction();
-    const ix = await depositTokensInstruction(
-      program,
-      stake_account_positions_secret.publicKey,
-      pyth_mint_account.publicKey,
-      101
-    );
-    transaction.add(ix);
-    const tx = await program.provider.send(transaction, [], {
-      skipPreflight: DEBUG,
-    });
+//   it("deposits tokens", async () => {
+//     const transaction = new Transaction();
+//     const ix = await depositTokensInstruction(
+//       program,
+//       stake_account_positions_secret.publicKey,
+//       pyth_mint_account.publicKey,
+//       101
+//     );
+//     transaction.add(ix);
+//     const tx = await program.provider.send(transaction, [], {
+//       skipPreflight: DEBUG,
+//     });
 
-    const to_account = (
-      await PublicKey.findProgramAddress(
-        [
-          anchor.utils.bytes.utf8.encode("custody"),
-          stake_account_positions_secret.publicKey.toBuffer(),
-        ],
-        program.programId
-      )
-    )[0];
+//     const to_account = (
+//       await PublicKey.findProgramAddress(
+//         [
+//           anchor.utils.bytes.utf8.encode("custody"),
+//           stake_account_positions_secret.publicKey.toBuffer(),
+//         ],
+//         program.programId
+//       )
+//     )[0];
 
-    const mint = new Token(
-      program.provider.connection,
-      pyth_mint_account.publicKey,
-      TOKEN_PROGRAM_ID,
-      new Keypair()
-    );
+//     const mint = new Token(
+//       program.provider.connection,
+//       pyth_mint_account.publicKey,
+//       TOKEN_PROGRAM_ID,
+//       new Keypair()
+//     );
 
-    assert.equal(
-      (await mint.getAccountInfo(to_account)).amount.toNumber(),
-      101
-    );
-  });
-});
+//     assert.equal(
+//       (await mint.getAccountInfo(to_account)).amount.toNumber(),
+//       101
+//     );
+//   });
+// });
 
