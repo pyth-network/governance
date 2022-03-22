941776c2791f4de9b21955b3424ed981e68280c8
Philip Taffet <ptaffet@users.noreply.github.com>
2022-03-21 22:34:11 -0500

    Restore conditional wasm import (#38)
    * await result of import
    
    * Update tsconfig as well
    
    * Works with both, but has race condition. Will discuss
    
    * Fix race condition

--- staking-ts/src/StakeConnection.ts
+++ staking-ts/src/StakeConnection.ts
@@ -9,24 +9,34 @@ import {
   SYSVAR_RENT_PUBKEY,
 } from "@solana/web3.js";
 
-import * as wasm from "../../staking/wasm/bundle/staking";
 
-// // This seems to work for now, but I am not sure how fragile it is.
-// const useNode =
-//   typeof process !== undefined && process.env.hasOwnProperty("_")
-//   && (!process.env._.includes("next"));
+// To prevent a potential race condition, prior to using the variable wasm,
+// you need to run:
+//        if (wasm == undefined)
+//           await ensureWasmLoaded;
+// (it's also okay to do it unconditionally)
+
+// This seems to work for now, but I am not sure how fragile it is.
+const useNode =
+  typeof process !== undefined && process.env.hasOwnProperty("_")
+  && (!process.env._.includes("next"));
 // console.log("Using node WASM version? " + useNode);
-// let wasm;
-// if (useNode) {
-//   // This needs to be sufficiently complicated that the bundler can't compute its value
-//   // It means the bundler will give us a warning "the request of a dependency is an expression"
-//   // because it doesn't understand that it will never encounter a case in which useNode is true.
-//   // When normal node is running, it doesn't care that this is an expression.
-//   const path = useNode ? "../../staking/wasm/" + "node" + "/staking" : "BAD";
-//   wasm = require(path);
-// } else {
-//   wasm = require("../../staking/wasm/bundle/staking");
-// }
+let wasm;
+let ensureWasmLoaded: Promise<void>;
+if (useNode) {
+  // This needs to be sufficiently complicated that the bundler can't compute its value
+  // It means the bundler will give us a warning "the request of a dependency is an expression"
+  // because it doesn't understand that it will never encounter a case in which useNode is true.
+  // When normal node is running, it doesn't care that this is an expression.
+  const path = useNode ? "../../staking/wasm/" + "node" + "/staking" : "BAD";
+  wasm = require(path);
+  ensureWasmLoaded = Promise.resolve();
+} else {
+  const f = async () => {
+    wasm = await require("../../staking/wasm/bundle/staking");
+  };
+  ensureWasmLoaded = f();
+}
 
 import { sha256 } from "js-sha256";
 import { bs58 } from "@project-serum/anchor/dist/cjs/utils/bytes";
@@ -105,6 +115,8 @@ export class StakeConnection {
   // }
 
   async fetchPositionAccount(address: PublicKey) {
+    if (wasm == undefined)
+      await ensureWasmLoaded;
     const inbuf = await this.program.provider.connection.getAccountInfo(
       address
     );
