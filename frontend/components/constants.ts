0796c6a4f3be6eb591f7d363b633bd018a9e7563
Daniel Chew <cctdaniel@outlook.com>
2022-03-25 18:45:30 +0800

    update constants

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -1,12 +1,16 @@
 import { PublicKey } from '@solana/web3.js'
 
-export const STAKING_PROGRAM = new PublicKey(
-  'Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS'
-)
+const LOCALNET_PROGRAM = new PublicKey(
+  'Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS')
+const DEVNET_PROGRAM = new PublicKey('CXJkmPRdCCsqdbqT5t8pw94VhrhiSnHuXLPR2dYjsFg')
 
-export const PYTH_MINT_ACCOUNT_PUBKEY = new PublicKey(
-  '2qF1zgC38b6LRa51G2NndLn8dxtE9AUassGyUJuw5nWh'
-)
+const LOCALNET_PYTH_MINT = new PublicKey('GiLLpVm6gWwM6kLbEFgQkwapg1j34YPs3dG6vjqG38Ag') // replace this whenever running locally
+const DEVNET_PYTH_MINT = new PublicKey('EceXQPnPa9kozZjDVW2LgNjew4aWNJpGCGCFuwJ8J4KB')
+
+export const STAKING_PROGRAM = process.env.ENDPOINT === 'localnet' ? LOCALNET_PROGRAM : DEVNET_PROGRAM
+
+export const PYTH_MINT_ACCOUNT_PUBKEY = process.env.ENDPOINT === 'localnet' ? LOCALNET_PYTH_MINT : DEVNET_PYTH_MINT
+
 
 export const STAKE_ACCOUNT_METADATA_SEED = 'stake_metadata'
 export const CUSTODY_SEED = 'custody'
