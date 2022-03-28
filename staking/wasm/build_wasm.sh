ed867fd3ad34730d67e0450a2fcc201578a1d7a1
Daniel Chew <cctdaniel@outlook.com>
2022-03-28 18:10:13 +0100

    add wasm dir

--- staking/wasm/build_wasm.sh
+++ staking/wasm/build_wasm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd programs/staking/
 which wasm-pack
+mkdir -p ../../wasm/bundle
+mkdir -p ../../wasm/node
 wasm-pack build  -d ../../wasm/bundle -- --features wasm
 wasm-pack build  -d ../../wasm/node -t nodejs -- --features wasm
