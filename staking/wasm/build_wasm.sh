687b0a880ca0b2f22a57bc3588d09446d09f3080
Daniel Chew <cctdaniel@outlook.com>
2022-03-28 18:13:48 +0100

    add more troubleshooting commands

--- staking/wasm/build_wasm.sh
+++ staking/wasm/build_wasm.sh
@@ -3,5 +3,8 @@ cd programs/staking/
 which wasm-pack
 mkdir -p ../../wasm/bundle
 mkdir -p ../../wasm/node
+ls ../../wasm/
+echo "##"
+ls *
 wasm-pack build  -d ../../wasm/bundle -- --features wasm
 wasm-pack build  -d ../../wasm/node -t nodejs -- --features wasm
