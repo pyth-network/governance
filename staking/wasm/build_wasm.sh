7c08ebbcccf506dae9d4ad12e983344159374e6e
Daniel Chew <cctdaniel@outlook.com>
2022-03-28 18:52:24 +0100

    revert build_wasm.sh changes

--- staking/wasm/build_wasm.sh
+++ staking/wasm/build_wasm.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd programs/staking/
-which wasm-pack
-mkdir -p ../../wasm/bundle
-mkdir -p ../../wasm/node
-ls ../../wasm/
-echo "##"
-ls *
-rustc --version
 wasm-pack build  -d ../../wasm/bundle -- --features wasm
 wasm-pack build  -d ../../wasm/node -t nodejs -- --features wasm
