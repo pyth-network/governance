19efc3bae79490c88cabd6a699abefa827d89fbf
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-03-30 17:00:35 +0100

    Add file for token representation conversions

---
+++ staking/app/token_decimals.ts
@@ -0,0 +1,13 @@
+import BN from 'bn.js';
+
+/* Solana token balances are represented by an integer (BN on typescript), but the actual balance
+is integer * 10 ** -decimals (a number on typescript) where decimals is a field of the token mint.
+These two functions are used to convert from one representation to the other.
+*/
+export function amountBnToNumber(amount: BN, decimals: number): number {
+    return amount.toNumber() * 10 ** -decimals;
+  }
+  
+  export function amountNumberToBn(amount: number, decimals: number): BN {
+    return new BN(amount).mul(new BN(10).pow(new BN(decimals)));
+  }
