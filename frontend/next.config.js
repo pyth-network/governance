6e10b9256d165bd41e7afbb3ced3102b0ce2734f
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-04-01 16:44:34 +0100

    npm package works

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,17 +14,7 @@ module.exports = {
     DEVNET_PYTH_MINT: process.env.DEVNET_PYTH_MINT,
   },
   webpack: (config, { isServer, dev }) => {
-    config.experiments = { asyncWebAssembly: true, topLevelAwait: true, layers: true, }
-
-    // Workaround from https://github.com/vercel/next.js/issues/29362#issuecomment-932767530
-    // In prod mode and in the server bundle (the place where this "chunks" bug
-    // appears), use the client static directory for the same .wasm bundle
-    config.output.webassemblyModuleFilename =
-      isServer && !dev ? "../static/wasm/[id].wasm" : "static/wasm/[id].wasm";
-
-    // Ensure the filename for the .wasm bundle is the same on both the client
-    // and the server (as in any other mode the ID's won't match)
-    config.optimization.moduleIds = "named";
+    config.experiments = { asyncWebAssembly: true, layers: true, }
 
     return config
   },
