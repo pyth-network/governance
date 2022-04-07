03a15cb93d00adb6649df6f591f2972a0545d34b
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-04-07 14:03:08 +0200

    Nice toString for PythBalance

--- staking/tests/pyth_balance.ts
+++ staking/tests/pyth_balance.ts
@@ -6,61 +6,66 @@ describe("pyth balance tests", async () => {
   it("Tests on 0", async () => {
     let amount = PythBalance.fromString("0");
     assert.equal(amount.toNumber(), 0);
-    assert.equal(amount.toString(), "0.000000");
+    assert.equal(amount.toString(), "0");
     assert(amount.eq(new PythBalance(new BN(0))));
     assert(amount.toBN().eq(new BN(0)));
 
     amount = new PythBalance(new BN(0));
     assert.equal(amount.toNumber(), 0);
-    assert.equal(amount.toString(), "0.000000");
+    assert.equal(amount.toString(), "0");
     assert(amount.eq(new PythBalance(new BN(0))));
     assert(amount.toBN().eq(new BN(0)));
 
     amount = PythBalance.fromNumber(0);
     assert.equal(amount.toNumber(), 0);
-    assert.equal(amount.toString(), "0.000000");
+    assert.equal(amount.toString(), "0");
     assert(amount.eq(new PythBalance(new BN(0))));
     assert(amount.toBN().eq(new BN(0)));
   });
 
   it("Tests on 0.1", async () => {
     let amount = PythBalance.fromString("0.10000");
-    assert.equal(amount.toString(), "0.100000");
+    assert.equal(amount.toString(), "0.1");
     assert(amount.eq(new PythBalance(new BN(100_000))));
     assert(amount.toBN().eq(new BN(100_000)));
 
     amount = PythBalance.fromString("0.1");
-    assert.equal(amount.toString(), "0.100000");
+    assert.equal(amount.toString(), "0.1");
+    assert(amount.eq(new PythBalance(new BN(100_000))));
+    assert(amount.toBN().eq(new BN(100_000)));
+
+    amount = PythBalance.fromString(".1");
+    assert.equal(amount.toString(), "0.1");
     assert(amount.eq(new PythBalance(new BN(100_000))));
     assert(amount.toBN().eq(new BN(100_000)));
 
     amount = new PythBalance(new BN(100_000));
-    assert.equal(amount.toString(), "0.100000");
+    assert.equal(amount.toString(), "0.1");
     assert(amount.eq(new PythBalance(new BN(100_000))));
     assert(amount.toBN().eq(new BN(100_000)));
   });
 
   it("Tests on 100", async () => {
     let amount = PythBalance.fromString("100.0");
-    assert.equal(amount.toString(), "100.000000");
+    assert.equal(amount.toString(), "100");
     assert.equal(amount.toNumber(), 100);
     assert(amount.eq(new PythBalance(new BN(100_000_000))));
     assert(amount.toBN().eq(new BN(100_000_000)));
 
     amount = PythBalance.fromString("100");
-    assert.equal(amount.toString(), "100.000000");
+    assert.equal(amount.toString(), "100");
     assert.equal(amount.toNumber(), 100);
     assert(amount.eq(new PythBalance(new BN(100_000_000))));
     assert(amount.toBN().eq(new BN(100_000_000)));
 
     amount = new PythBalance(new BN(100_000_000));
-    assert.equal(amount.toString(), "100.000000");
+    assert.equal(amount.toString(), "100");
     assert.equal(amount.toNumber(), 100);
     assert(amount.eq(new PythBalance(new BN(100_000_000))));
     assert(amount.toBN().eq(new BN(100_000_000)));
 
     amount = PythBalance.fromNumber(100);
-    assert.equal(amount.toString(), "100.000000");
+    assert.equal(amount.toString(), "100");
     assert.equal(amount.toNumber(), 100);
     assert(amount.eq(new PythBalance(new BN(100_000_000))));
     assert(amount.toBN().eq(new BN(100_000_000)));
