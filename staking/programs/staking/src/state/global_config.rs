5895dbd61bc661b3553b9b24adaaaa2aaf428b45
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-04-12 10:04:15 -0500

    Fix format, last time

--- staking/programs/staking/src/state/global_config.rs
+++ staking/programs/staking/src/state/global_config.rs
@@ -3,14 +3,10 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(Default)]
 pub struct GlobalConfig {
-    pub bump:                  u8,
-    pub governance_authority:  Pubkey,
-    pub pyth_token_mint:       Pubkey,
-    
-    
-    
-    
-    
+    pub bump:                 u8,
+    pub governance_authority: Pubkey,
+    pub pyth_token_mint:      Pubkey,
+
     pub pyth_governance_realm: Pubkey,
     pub unlocking_duration:    u8,
     pub epoch_duration:        u64, // epoch duration in seconds
