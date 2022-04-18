136937ed6b15bfa7f749353c9b1a1935ce2fcd8e
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-04-18 15:40:01 -0500

    Add constants file

---
+++ staking/app/constants.ts
@@ -0,0 +1,9 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEVNET_ENDPOINT = "https://api.devnet.solana.com";
+export const DEVNET_GOVERNANCE_ADDRESS = new PublicKey(
+  "GTesTBiEWE32WHXXE2S4XbZvA5CrEc4xs6ZgRe895dP"
+);
+export const LOCALNET_GOVERNANCE_ADDRESS = new PublicKey(
+  "pythGovernance11111111111111111111111111111"
+);
