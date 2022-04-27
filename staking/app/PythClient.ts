2b14e0e3c62f04c9fa14a9509966c4e83dadf2b5
Daniel Chew <cctdaniel@outlook.com>
2022-04-28 02:59:41 +0900

    update pyth client

--- staking/app/PythClient.ts
+++ staking/app/PythClient.ts
@@ -2,11 +2,14 @@ import { Wallet } from "@project-serum/anchor";
 import { Provider } from "@project-serum/common";
 import { PublicKey } from "@solana/web3.js";
 import { DEVNET_STAKING_ADDRESS, LOCALNET_STAKING_ADDRESS } from "./constants";
-import { StakeConnection } from "./StakeConnection";
+import { PythBalance } from "./pythBalance";
+import { StakeAccount, StakeConnection } from "./StakeConnection";
 
 export class PythClient {
   program: { programId: PublicKey };
   cluster: string;
+  stakeAccount: StakeAccount;
+  voterWeight: PythBalance;
   constructor(public stakeConnection: StakeConnection, cluster: string) {
     this.cluster = cluster;
     this.program = {
@@ -15,6 +18,8 @@ export class PythClient {
           ? LOCALNET_STAKING_ADDRESS
           : DEVNET_STAKING_ADDRESS,
     };
+    this.stakeAccount = null;
+    this.voterWeight = PythBalance.fromString("0");
   }
   static async connect(
     provider: Provider,
@@ -22,15 +27,18 @@ export class PythClient {
   ): Promise<PythClient> {
     // only supports localnet and devnet for now
     // TODO: update this to support mainnet when program is deployed
-    return new PythClient(
-      await StakeConnection.createStakeConnection(
-        provider.connection,
-        provider.wallet as unknown as Wallet,
-        cluster === "localnet"
-          ? LOCALNET_STAKING_ADDRESS
-          : DEVNET_STAKING_ADDRESS
-      ),
-      cluster
+    const stakeConnection = await StakeConnection.createStakeConnection(
+      provider.connection,
+      provider.wallet as unknown as Wallet,
+      cluster === "localnet" ? LOCALNET_STAKING_ADDRESS : DEVNET_STAKING_ADDRESS
+    );
+    return new PythClient(stakeConnection, cluster);
+  }
+
+  public async update(publicKey: PublicKey) {
+    this.stakeAccount = await this.stakeConnection.getMainAccount(publicKey);
+    this.voterWeight = this.stakeAccount?.getVoterWeight(
+      await this.stakeConnection.getTime()
     );
   }
 }
