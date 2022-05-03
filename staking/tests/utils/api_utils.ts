d33594b4b5a84e114875a85070cdf6a0bb10ebca
Philip Taffet <ptaffet@rice.edu>
2022-05-03 14:11:18 -0500

    Add fake proposals, use them for testing voter weight

--- staking/tests/utils/api_utils.ts
+++ staking/tests/utils/api_utils.ts
@@ -1,13 +1,35 @@
-import { StakeConnection, BalanceSummary } from "../../app/StakeConnection";
-import { Keypair, PublicKey, Transaction } from "@solana/web3.js";
+import { StakeConnection } from "../../app/StakeConnection";
+import {
+  BpfLoader,
+  BPF_LOADER_PROGRAM_ID,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  TransactionInstruction,
+} from "@solana/web3.js";
 import assert from "assert";
 import BN from "bn.js";
 import { PythBalance } from "../../app";
-import * as wasm from "../../wasm/node/staking";
-import * as anchor from "@project-serum/anchor";
 import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import * as gov from "@solana/spl-governance";
+import {
+  Proposal,
+  getGovernanceSchemaForAccount,
+  GovernanceAccountType,
+  ProposalState,
+  BN_ZERO,
+  VoteType,
+  InstructionExecutionFlags,
+  VoteThresholdPercentage,
+  VoteTypeKind,
+  BPF_UPGRADE_LOADER_ID,
+} from "@solana/spl-governance";
+import { serialize, BinaryWriter } from "borsh";
 
+const SPL_SHARED_MEMORY_ID = new PublicKey(
+  "shmem4EWT2sPdVGvTZCzXXRAURL9G5vpPxNwSeKhHUL"
+);
 /**
  * Like BalanceSummary, but all fields are optional. If they aren't given, it's equivalent to them being specified as 0.
  */
@@ -88,21 +110,153 @@ async function assertVoterWeightEqualsAt(
   const pythMintSupply: BN = pythMintInfo.supply;
 
   // First check expected matches the WASM-computed value
-  const currentActual = await stakeAccount.getVoterWeight(time);
+
   let expectedScaled = new BN(0);
   if (expected.totalLockedBalance.toBN().gtn(0))
     expectedScaled = expected.rawVoterWeight
       .toBN()
       .mul(pythMintSupply)
       .div(expected.totalLockedBalance.toBN());
+  const currentActual = await stakeAccount.getVoterWeight(time);
   assert.equal(currentActual.toBN().toString(), expectedScaled.toString());
 
-  // // Now create a fake proposal, update the voter weight, and make sure the voter record matches expected
-  // const tx = new Transaction();
-  // const fns = gov.getGovernanceSchemaForAccount(
-  //   gov.GovernanceAccountType.GovernanceV2
-  // );
-  // console.dir(fns);
+  const proposalAddress =
+    await new MockProposalCreator().createMockProposalWithTime(
+      stakeConnection,
+      time
+    );
+
+  const tx = new Transaction();
+  const { voterWeightAccount } = await stakeConnection.withUpdateVoterWeight(
+    tx.instructions,
+    stakeAccount,
+    { castVote: {} },
+    proposalAddress
+  );
+  await stakeConnection.provider.sendAndConfirm(tx);
+
+  const onChain = await stakeConnection.program.account.voterWeightRecord.fetch(
+    voterWeightAccount
+  );
+  assert.equal(onChain.voterWeight.toString(), expectedScaled.toString());
+  assert.equal(
+    onChain.weightActionTarget.toBase58(),
+    proposalAddress.toBase58()
+  );
+}
+
+class MockProposalCreator {
+  static cache: Map<BN, PublicKey>;
+
+  public async getMockProposalWithTime(
+    stakeConnection: StakeConnection,
+    time: BN
+  ) {
+    if (MockProposalCreator.cache.has(time))
+      return MockProposalCreator.cache.get(time);
+    const proposal = await this.createMockProposalWithTime(
+      stakeConnection,
+      time
+    );
+    MockProposalCreator.cache.set(time, proposal);
+    return proposal;
+  }
+
+  async createMockProposalWithTime(
+    stakeConnection: StakeConnection,
+    time: BN
+  ): Promise<PublicKey> {
+    let proposal: Proposal = new Proposal({
+      accountType: GovernanceAccountType.ProposalV2,
+      governance: PublicKey.default,
+      governingTokenMint: stakeConnection.config.pythTokenMint,
+      state: ProposalState.Voting,
+      tokenOwnerRecord: PublicKey.default,
+      signatoriesCount: 0,
+      signatoriesSignedOffCount: 0,
+      descriptionLink: "Fake description",
+      name: "Fake proposal",
+      yesVotesCount: BN_ZERO,
+      noVotesCount: BN_ZERO,
+      instructionsExecutedCount: 0,
+      instructionsCount: 0,
+      instructionsNextIndex: 0,
+      voteType: VoteType.SINGLE_CHOICE,
+      options: [],
+      denyVoteWeight: undefined,
+      vetoVoteWeight: undefined,
+      abstainVoteWeight: undefined,
+      startVotingAt: null,
+      maxVotingTime: null,
+      draftAt: BN_ZERO,
+      signingOffAt: null,
+      votingAt: time,
+      votingAtSlot: null,
+      votingCompletedAt: null,
+      executingAt: null,
+      closedAt: null,
+      executionFlags: InstructionExecutionFlags.None,
+      maxVoteWeight: null,
+      voteThresholdPercentage: new VoteThresholdPercentage({ value: 50 }),
+    });
+    (proposal as any).reserved = new Array(64); // Gross...
+    const schema = getGovernanceSchemaForAccount(
+      GovernanceAccountType.ProposalV2
+    );
+    const serializedProp = serialize(
+      schema,
+      proposal,
+      this.GovernanceBinaryWriter
+    );
+    const sharedMemData = new BinaryWriter();
+    sharedMemData.writeU64(0); // Offset
+    sharedMemData.writeFixedArray(serializedProp);
+
+    // BpfLoader.load requires the fee payer to be a Keypair, so we need to transfer some Sol to a new fee-payer
+    const feePayer = Keypair.generate();
+    const tx1 = new Transaction();
+    tx1.add(
+      SystemProgram.transfer({
+        fromPubkey: stakeConnection.provider.wallet.publicKey,
+        toPubkey: feePayer.publicKey,
+        lamports: LAMPORTS_PER_SOL,
+      })
+    );
+    await stakeConnection.provider.sendAndConfirm(tx1);
+
+    const proposalAddress = Keypair.generate();
+    try {
+      await BpfLoader.load(
+        stakeConnection.provider.connection,
+        feePayer,
+        proposalAddress,
+        serializedProp,
+        BPF_LOADER_PROGRAM_ID
+      );
+    } catch (error) {
+      // This fails because it's not an ELF, but at that point it has already populated the account.
+      // Gross, but I think this is the easiest way
+    }
+    return proposalAddress.publicKey;
+  }
+  // Uggh gross. In order to serialize this, we need to take a few functions from various parts of the governance API package
+  GovernanceBinaryWriter = class extends BinaryWriter {
+    writePubkey(value: PublicKey) {
+      this.maybeResize();
+      this.writeFixedArray(value.toBuffer());
+    }
+    writeVoteType(value: VoteType) {
+      const writer = this as unknown as BinaryWriter;
+      writer.maybeResize();
+      writer.buf.writeUInt8(value.type, writer.length);
+      writer.length += 1;
+
+      if (value.type === VoteTypeKind.MultiChoice) {
+        writer.buf.writeUInt16LE(value.choiceCount!, writer.length);
+        writer.length += 2;
+      }
+    }
+  };
 }
 
 export async function assertVoterWeightEquals(
