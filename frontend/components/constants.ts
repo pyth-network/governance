112c0be2acc53b979fca3c20ebe34264d4f882ee
Daniel Chew <cctdaniel@outlook.com>
2022-05-05 14:38:27 +0900

    update frontend to use api devnet and localnet staking address

--- frontend/components/constants.ts
+++ frontend/components/constants.ts
@@ -1,13 +1,15 @@
 import { PublicKey } from '@solana/web3.js'
-
-const LOCALNET_PROGRAM = new PublicKey(process.env.LOCALNET_PROGRAM!)
-const DEVNET_PROGRAM = new PublicKey(process.env.DEVNET_PROGRAM!)
-
-const LOCALNET_PYTH_MINT = new PublicKey(process.env.LOCALNET_PYTH_MINT!)
-const DEVNET_PYTH_MINT = new PublicKey(process.env.DEVNET_PYTH_MINT!)
+import {
+  DEVNET_STAKING_ADDRESS,
+  LOCALNET_STAKING_ADDRESS,
+} from 'pyth-staking-api'
 
 export const STAKING_PROGRAM =
-  process.env.ENDPOINT === 'devnet' ? DEVNET_PROGRAM : LOCALNET_PROGRAM
+  process.env.ENDPOINT === 'devnet'
+    ? DEVNET_STAKING_ADDRESS
+    : LOCALNET_STAKING_ADDRESS
 
 export const PYTH_MINT_ACCOUNT_PUBKEY =
-  process.env.ENDPOINT === 'devnet' ? DEVNET_PYTH_MINT : LOCALNET_PYTH_MINT
+  process.env.ENDPOINT === 'devnet'
+    ? new PublicKey(process.env.DEVNET_PYTH_MINT!)
+    : new PublicKey(process.env.LOCALNET_PYTH_MINT!)
