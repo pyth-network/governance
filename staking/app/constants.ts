40bea6c1d503785043d1867cafa1c1df84701387
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-05-04 23:00:28 -0500

    Update governance address for devnet

--- staking/app/constants.ts
+++ staking/app/constants.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 
 export const DEVNET_ENDPOINT = "https://api.devnet.solana.com";
 export const DEVNET_GOVERNANCE_ADDRESS = new PublicKey(
-  "GTesTBiEWE32WHXXE2S4XbZvA5CrEc4xs6ZgRe895dP"
+  "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw"
 );
 export const LOCALNET_GOVERNANCE_ADDRESS = new PublicKey(
   "GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw"
