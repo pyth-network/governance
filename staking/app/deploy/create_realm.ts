5dde93bc8ed4b1473ffc19403db949feab84032d
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-10-18 13:10:32 -0500

    Update create_realm

--- staking/app/deploy/create_realm.ts
+++ staking/app/deploy/create_realm.ts
@@ -1,9 +1,11 @@
 import {
+  GoverningTokenType,
   MintMaxVoteWeightSource,
   PROGRAM_VERSION_V2,
   withCreateRealm,
 } from "@solana/spl-governance";
 import { Transaction, Connection } from "@solana/web3.js";
+import { BN } from "bn.js";
 
 import {
   AUTHORITY_KEYPAIR,
@@ -11,23 +13,32 @@ import {
   STAKING_PROGRAM,
   GOVERNANCE_PROGRAM,
   RPC_NODE,
-} from "./devnet";
+} from "./mainnet_beta";
+
+import { Constants } from "pyth-staking-wasm";
 
 async function main() {
   const tx = new Transaction();
+
   await withCreateRealm(
     tx.instructions,
-    GOVERNANCE_PROGRAM,
-    PROGRAM_VERSION_V2,
-    "Pyth Governance",
-    AUTHORITY_KEYPAIR.publicKey,
-    PYTH_TOKEN,
-    AUTHORITY_KEYPAIR.publicKey,
-    undefined, // no council mint
-    MintMaxVoteWeightSource.FULL_SUPPLY_FRACTION,
-    MintMaxVoteWeightSource.SUPPLY_FRACTION_BASE, // Full token supply required to create a gov, i.e. only realmAuth can do it
-    STAKING_PROGRAM,
-    undefined // new PublicKey(config.programs.localnet.staking) //TODO: Restore after max voter weight plugin implemented
+    GOVERNANCE_PROGRAM, // Address of the governance program
+    PROGRAM_VERSION_V2, // Version of the on-chain governance program
+    "Pyth Governance", // `name` of the realm
+    AUTHORITY_KEYPAIR.publicKey, // Address of the realm authority
+    PYTH_TOKEN, // Address of the Pyth token
+    AUTHORITY_KEYPAIR.publicKey, // Address of the payer
+    undefined, // No council mint
+    MintMaxVoteWeightSource.FULL_SUPPLY_FRACTION, // Irrelevant because we use the max voter weight plugin
+    new BN(
+      Constants.MAX_VOTER_WEIGHT().toString() // Create governance requires realm authority
+    ),
+    {
+      voterWeightAddin: STAKING_PROGRAM, // Voter weight plugin
+      maxVoterWeightAddin: STAKING_PROGRAM, // Max voter weight plugin
+      tokenType: GoverningTokenType.Liquid, // Liquid token
+    },
+    undefined // No council mint
   );
 
   const client = new Connection(RPC_NODE);
