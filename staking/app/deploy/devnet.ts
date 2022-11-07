b498cf3b8bfffab06a7536c878bed4bff7ef2e9c
guibescos <59208140+guibescos@users.noreply.github.com>
2022-11-07 09:12:36 -0600

    Transfer keys to multisig (#211)

--- staking/app/deploy/devnet.ts
+++ staking/app/deploy/devnet.ts
@@ -1,7 +1,6 @@
 import { PublicKey, Keypair } from "@solana/web3.js";
 import fs from "fs";
 import { homedir } from "os";
-
 export const AUTHORITY_PATH = "/.config/solana/deployer.json";
 export const AUTHORITY_KEYPAIR = Keypair.fromSecretKey(
   new Uint8Array(
@@ -9,13 +8,14 @@ export const AUTHORITY_KEYPAIR = Keypair.fromSecretKey(
   )
 );
 
+export const MULTISIG_AUTHORITY = new PublicKey(
+  "7g4Los4WMQnpxYiBJpU1HejBiM6xCk5RDFGCABhWE9M6"
+);
+
 export const PYTH_TOKEN = new PublicKey(
   "7Bd6bEH4wHTMmov8D2WTXgxzLJcxJYczqE5NaDtZdhF6"
 );
 
-export const REALM = new PublicKey(
-  "44xGQELUXXD1TiLEMc73RBnCxeW8XKw27LyJNpt2G8bF"
-);
 export const RPC_NODE = "https://api.devnet.solana.com";
 
 export const EPOCH_DURATION = 3600;
