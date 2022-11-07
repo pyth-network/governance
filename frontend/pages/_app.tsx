d354b132274e8e88f21b64082af18f3c79b86fdc
Daniel Chew <cctdaniel@outlook.com>
2022-11-07 16:28:52 +0000

    replace slope wallet adapter with backpack

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import type { AppProps } from 'next/app'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import {
   ConnectionProvider,
@@ -7,19 +5,19 @@ import {
 } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import {
+  BackpackWalletAdapter,
   GlowWalletAdapter,
   LedgerWalletAdapter,
   PhantomWalletAdapter,
-  SlopeWalletAdapter,
   SolflareWalletAdapter,
   SolletExtensionWalletAdapter,
   SolletWalletAdapter,
   TorusWalletAdapter,
 } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
-import { Toaster } from 'react-hot-toast'
+import type { AppProps } from 'next/app'
 import { FC, useMemo } from 'react'
-import Footer from '@components/Footer'
+import { Toaster } from 'react-hot-toast'
 
 // Use require instead of import since order matters
 require('@solana/wallet-adapter-react-ui/styles.css')
@@ -32,8 +30,7 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   // You can also provide a custom RPC endpoint
   // const endpoint = useMemo(() => clusterApiUrl(network), [network])
 
-  const endpoint =
-    process.env.ENDPOINT
+  const endpoint = process.env.ENDPOINT
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
   // of wallets that your users connect to will be loaded
@@ -41,7 +38,7 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
     () => [
       new PhantomWalletAdapter(),
       new GlowWalletAdapter(),
-      new SlopeWalletAdapter(),
+      new BackpackWalletAdapter(),
       new SolflareWalletAdapter(),
       new TorusWalletAdapter(),
       new LedgerWalletAdapter(),
