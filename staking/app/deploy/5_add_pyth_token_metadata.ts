b340538978ba3a9cc0b8f653a1c745e9ea0f9830
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-11-08 12:15:20 +0000

    Add token metadata

--- staking/app/deploy/5_add_pyth_token_metadata.ts
+++ staking/app/deploy/5_add_pyth_token_metadata.ts
@@ -6,35 +6,41 @@ import {
   DataV2,
 } from "@metaplex-foundation/mpl-token-metadata";
 
-const PYTH_ONCHAIN_METADATA = {
-  name: "Pyth",
-  symbol: "PYTH",
-  uri: "https://arweave.net/V-UQtAKq6zfbVC7C7vkgjgxAc5lnJ6dAyXNs8MQrXyY",
-  sellerFeeBasisPoints: 0,
-  creators: null,
-  collection: null,
-  uses: null,
-} as DataV2;
+async function main() {
+  const PYTH_ONCHAIN_METADATA = {
+    name: "Pyth",
+    symbol: "PYTH",
+    uri: "https://arweave.net/V-UQtAKq6zfbVC7C7vkgjgxAc5lnJ6dAyXNs8MQrXyY",
+    sellerFeeBasisPoints: 0,
+    creators: null,
+    collection: null,
+    uses: null,
+  } as DataV2;
 
-const client = new Connection(RPC_NODE);
-const metaplex = Metaplex.make(client);
-const metadataPDA = metaplex.nfts().pdas().metadata({ mint: PYTH_TOKEN });
+  const client = new Connection(RPC_NODE);
+  const metaplex = Metaplex.make(client);
+  const metadataPDA = metaplex.nfts().pdas().metadata({ mint: PYTH_TOKEN });
 
-const tx = new Transaction();
-tx.instructions.push(
-  createCreateMetadataAccountV2Instruction(
-    {
-      metadata: metadataPDA,
-      mint: PYTH_TOKEN,
-      mintAuthority: AUTHORITY_KEYPAIR.publicKey,
-      payer: AUTHORITY_KEYPAIR.publicKey,
-      updateAuthority: AUTHORITY_KEYPAIR.publicKey,
-    },
-    {
-      createMetadataAccountArgsV2: {
-        data: PYTH_ONCHAIN_METADATA,
-        isMutable: true,
+  const tx = new Transaction();
+  tx.instructions.push(
+    createCreateMetadataAccountV2Instruction(
+      {
+        metadata: metadataPDA,
+        mint: PYTH_TOKEN,
+        mintAuthority: AUTHORITY_KEYPAIR.publicKey,
+        payer: AUTHORITY_KEYPAIR.publicKey,
+        updateAuthority: AUTHORITY_KEYPAIR.publicKey,
       },
-    }
-  )
-);
+      {
+        createMetadataAccountArgsV2: {
+          data: PYTH_ONCHAIN_METADATA,
+          isMutable: true,
+        },
+      }
+    )
+  );
+
+  await client.sendTransaction(tx, [AUTHORITY_KEYPAIR]);
+}
+
+main();
