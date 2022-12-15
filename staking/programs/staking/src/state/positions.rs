fef9641deab17bd0067b34a564c2d734a6620e71
Jayant Krishnamurthy <jkrishnamurthy@jumptrading.com>
2022-12-15 06:25:00 -0800

    clippy

--- staking/programs/staking/src/state/positions.rs
+++ staking/programs/staking/src/state/positions.rs
@@ -206,7 +206,7 @@ pub enum PositionState {
 
 impl std::fmt::Display for PositionState {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
