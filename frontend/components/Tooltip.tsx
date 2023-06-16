431fd79de36fac9a343bcc96e17d8556e7ee820c
Daniel Chew <cctdaniel@outlook.com>
2023-06-16 12:07:50 +0900

    fix build error

--- frontend/components/Tooltip.tsx
+++ frontend/components/Tooltip.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode } from 'react'
 import Tippy from '@tippyjs/react'
+import React, { ReactNode } from 'react'
 import 'tippy.js/animations/scale.css'
 
 type TooltipProps = {
@@ -39,7 +39,7 @@ const Tooltip = ({
   )
 }
 
-const Content: React.FC<{ className: string }> = ({
+const Content: React.FC<{ className: string; children: React.ReactNode }> = ({
   className = '',
   children,
 }) => {
