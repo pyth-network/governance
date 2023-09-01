7f80673b4d336e31da43abbc29a26393abc2d5e7
Guillermo Bescos <g.bescos@yahoo.com>
2023-09-02 00:01:40 +0100

    Big time hack:

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -13,7 +15,18 @@ module.exports = {
   },
   webpack: (config, { isServer, dev }) => {
     config.experiments = { asyncWebAssembly: true, layers: true }
-
+    config.resolve.alias = {
+      ...config.resolve.alias,
+        "./staking_bg.wasm": path.resolve(__dirname, "../staking/app/wasm/bundle/staking_bg.wasm")
+    }
+    config.plugins = [
+      ...config.plugins,
+      new CopyPlugin({
+        patterns: [
+          { from: path.resolve(__dirname, "../wasm/bundle/staking_bg.wasm"), to: path.resolve(__dirname, "./.next/server/pages/staking_bg.wasm") },
+        ],
+      }),
+    ]
     return config
   },
   async redirects() {
