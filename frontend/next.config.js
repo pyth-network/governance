69d7b62803a41bc1aa0cfd875178b0d6b3232ad9
Guillermo Bescos <g.bescos@yahoo.com>
2023-09-12 17:27:05 +0100

    Revert these files

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,5 @@
-require("dotenv").config()
+require('dotenv').config()
+const path = require('path');
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -6,13 +7,27 @@ module.exports = {
   experimental: {
     externalDir: true,
   },
+  swcMinify: false,
   env: {
     ENDPOINT: process.env.ENDPOINT,
-    CLUSTER : process.env.CLUSTER
+    CLUSTER: process.env.CLUSTER,
   },
-  webpack: (config, { isServer, dev }) => {
-    config.experiments = { asyncWebAssembly: true, layers: true, }
+  webpack(config, { isServer }) {
+    config.experiments = { asyncWebAssembly: true, layers : true };
+    // This is hack to fix the import of the wasm files https://github.com/vercel/next.js/issues/25852
+    if (isServer) {
+      config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    } else {
+      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    }
+    config.optimization.moduleIds = 'named';
+    // End of hack
 
+    // Import the browser version of wasm instead of the node version
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "pyth-staking-wasm$": path.resolve(__dirname, '../wasm/bundle'),
+    }
     return config
   },
   async redirects() {
