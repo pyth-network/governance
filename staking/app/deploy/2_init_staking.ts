24af24e514a08ffb688e74316de737aff239dd06
Guillermo Bescos <g.bescos@yahoo.com>
2023-10-24 18:23:03 +0100

    Devnet done

--- staking/app/deploy/2_init_staking.ts
+++ staking/app/deploy/2_init_staking.ts
@@ -3,9 +3,15 @@ import { Connection } from "@solana/web3.js";
 import { getTargetAccount } from "../../tests/utils/utils";
 import { AUTHORITY_KEYPAIR, PYTH_TOKEN, RPC_NODE } from "./devnet";
 import { BN } from "bn.js";
-import { STAKING_ADDRESS, REALM_ID, EPOCH_DURATION } from "../constants";
+import {
+  STAKING_ADDRESS,
+  REALM_ID,
+  EPOCH_DURATION,
+  GOVERNANCE_ADDRESS,
+} from "../constants";
 
 // Actual transaction hash :
+// devnet (24/10/23): 4LDMVLijZsD3SeDMqeUZZ9mAov1TwyRJs96yuKztd7Cmv2p9ASWuP9JQXpL9fnr3eQc3gtxJqyWDZY1D7gj2NY6j
 // mainnet-beta : KrWZD8gbH6Afg6suwHrmUi1xDo25rLDqqMAoAdunXmtUmuVk5HZgQvDqxFHC2uidL6TfXSmwKdQnkbnbZc8BZam
 
 async function main() {
@@ -20,11 +26,15 @@ async function main() {
 
   const globalConfig = {
     governanceAuthority: AUTHORITY_KEYPAIR.publicKey,
-    pythGovernanceRealm: REALM_ID,
     pythTokenMint: PYTH_TOKEN,
+    pythGovernanceRealm: REALM_ID,
     unlockingDuration: 1,
     epochDuration: new BN(EPOCH_DURATION),
     freeze: false,
+    pdaAuthority: AUTHORITY_KEYPAIR.publicKey,
+    governanceProgram: GOVERNANCE_ADDRESS(),
+    pythTokenListTime: null,
+    agreementHash: Array.from(Buffer.alloc(0)),
   };
   await program.methods.initConfig(globalConfig).rpc();
 
