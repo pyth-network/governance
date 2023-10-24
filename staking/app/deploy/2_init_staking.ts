5e419a6667821351fa4a6abca4c72cdc746f6c20
Guillermo Bescos <g.bescos@yahoo.com>
2023-10-24 18:39:23 +0100

    Let's go

--- staking/app/deploy/2_init_staking.ts
+++ staking/app/deploy/2_init_staking.ts
@@ -1,7 +1,7 @@
 import { Wallet, AnchorProvider, Program } from "@project-serum/anchor";
 import { Connection } from "@solana/web3.js";
 import { getTargetAccount } from "../../tests/utils/utils";
-import { AUTHORITY_KEYPAIR, PYTH_TOKEN, RPC_NODE } from "./devnet";
+import { AUTHORITY_KEYPAIR, PYTH_TOKEN, RPC_NODE } from "./mainnet_beta";
 import { BN } from "bn.js";
 import {
   STAKING_ADDRESS,
@@ -12,7 +12,7 @@ import {
 
 // Actual transaction hash :
 // devnet (24/10/23): 4LDMVLijZsD3SeDMqeUZZ9mAov1TwyRJs96yuKztd7Cmv2p9ASWuP9JQXpL9fnr3eQc3gtxJqyWDZY1D7gj2NY6j
-// mainnet-beta : KrWZD8gbH6Afg6suwHrmUi1xDo25rLDqqMAoAdunXmtUmuVk5HZgQvDqxFHC2uidL6TfXSmwKdQnkbnbZc8BZam
+// mainnet-beta (24/10/23): 3zNHezg9tW3uEEwU4ALQK6Ux3X7zYP4UKcbFhLHrPM4VkxNiQTTVbijdCtqVM2PFA4ZrAVc4LBKaKe8CDVGVkQJY
 
 async function main() {
   const client = new Connection(RPC_NODE);
