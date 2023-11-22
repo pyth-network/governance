3113e4e87433410f861452dbb6b17452deb9bfc3
Guillermo Bescos <g.bescos@yahoo.com>
2023-11-22 17:38:37 +0000

    Next

--- frontend/pages/api/vesting/v1/vesting_accounts.ts
+++ frontend/pages/api/vesting/v1/vesting_accounts.ts
@@ -1,4 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { PythBalance } from '@pythnetwork/staking/app/pythBalance'
+import BN from 'bn.js'
 
 export default async function handlerVestingAccounts(
   req: NextApiRequest,
@@ -6,11 +8,13 @@ export default async function handlerVestingAccounts(
 ) {
   const { owner } = req.query
 
+  const balance = new PythBalance(new BN(1000))
+
   if (owner == undefined || owner instanceof Array) {
     res.status(400).json({
       error: "Must provide the 'owner' query parameters",
     })
   } else {
-    res.status(200).json({ owner: owner })
+    res.status(200).json({ owner: balance.toString() })
   }
 }
