209cdf9bd91f23fc1196ffeae34bbd48bc0ec17a
Guillermo Bescos <g.bescos@yahoo.com>
2023-11-22 17:31:19 +0000

    Another test

--- frontend/pages/api/vesting/v1/vesting_accounts.ts
+++ frontend/pages/api/vesting/v1/vesting_accounts.ts
@@ -1,10 +1,4 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { Provider } from '@project-serum/anchor'
-import { Connection, Keypair, PublicKey } from '@solana/web3.js'
-import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet'
-import { PythBalance } from '@pythnetwork/staking'
-
-const connection = new Connection(process.env.ENDPOINT!)
 
 export default async function handlerVestingAccounts(
   req: NextApiRequest,
@@ -17,7 +11,6 @@ export default async function handlerVestingAccounts(
       error: "Must provide the 'owner' query parameters",
     })
   } else {
-    const balance = PythBalance.fromString('1')
-    res.status(200).json({ balance: balance.toString() })
+    res.status(200).json({ owner: owner })
   }
 }
