7a3613d75fc5d35242ddc1d09b9062e4b375c97b
Guillermo Bescos <g.bescos@yahoo.com>
2023-11-30 20:23:40 +0000

    Rever this change

--- staking/app/StakeConnection.ts
+++ staking/app/StakeConnection.ts
@@ -1021,19 +1021,12 @@ export class StakeAccount {
     let unlockingDuration = this.config.unlockingDuration;
     let currentEpochBI = BigInt(currentEpoch.toString());
 
-    let withdrawable: BigInt;
-    try {
-      withdrawable = this.stakeAccountPositionsWasm.getWithdrawable(
-        BigInt(this.tokenBalance.toString()),
-        unvestedBalance,
-        currentEpochBI,
-        unlockingDuration
-      );
-    } catch (e) {
-      throw Error(
-        "This account has less tokens than the unlocking schedule or your staking position requires. Please contact support."
-      );
-    }
+    const withdrawable = this.stakeAccountPositionsWasm.getWithdrawable(
+      BigInt(this.tokenBalance.toString()),
+      unvestedBalance,
+      currentEpochBI,
+      unlockingDuration
+    );
 
     const withdrawableBN = new BN(withdrawable.toString());
     const unvestedBN = new BN(unvestedBalance.toString());
