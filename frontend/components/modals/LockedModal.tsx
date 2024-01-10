a02d278e04275326ffcc9f07517234bf8b4f07f4
Guillermo Bescos <g.bescos@yahoo.com>
2024-01-10 14:00:33 +0000

    Refactor

--- frontend/components/modals/LockedModal.tsx
+++ frontend/components/modals/LockedModal.tsx
@@ -15,6 +15,7 @@ import { MainStakeAccount } from 'pages'
 import { useState } from 'react'
 import { AmountInput } from '@components/panels/components'
 import { validAmountChange } from 'utils/validAmountChange'
+import { useUnlockMutation } from 'hooks/useUnlockMutation'
 
 export type LockedModalProps = {
   isLockedModalOpen: boolean
@@ -30,15 +31,6 @@ export function LockedModal({
 }: LockedModalProps) {
   const { data: balanceData, isLoading: _isBalanceLoading } =
     useBalance(mainStakeAccount)
-
-  const [amount, setAmount] = useState<string>('')
-  const handleAmountChange = (amount: string) => {
-    console.log(amount)
-    if (validAmountChange(amount)) {
-      console.log(validAmountChange(amount))
-      setAmount(amount)
-    }
-  }
 
   const {
     unvestedTotalPythBalance = PythBalance.zero(),
@@ -80,7 +72,6 @@ export function LockedModal({
       </p>
 
       <div className="flex flex-col items-center  space-y-4 text-center md:block md:space-x-10">
-        <AmountInput amount={amount} onAmountChange={handleAmountChange} />
         <LockedModalButton
           currentVestingAccountState={currentVestingAccountState}
           mainStakeAccount={mainStakeAccount}
