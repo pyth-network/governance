171565d514469f26d18e5a0cdcce12247d1ac8cf
Daniel Chew <cctdaniel@outlook.com>
2024-01-17 17:21:56 +0000

    fix mobile display

--- frontend/components/panels/components/Description.tsx
+++ frontend/components/panels/components/Description.tsx
@@ -1,3 +1,3 @@
 export function Description({ children }: { children: string }) {
-  return <div className="mb-4 h-36  sm:mb-12 sm:h-16">{children}</div>
+  return <div className="mb-4 sm:mb-12">{children}</div>
 }
