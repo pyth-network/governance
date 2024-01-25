5479659a6e012050d173ce76a8f098c0b996e1c3
Guillermo Bescos <g.bescos@yahoo.com>
2024-01-25 19:45:35 +0000

    Clean it

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,14 +12,12 @@ module.exports = {
     ENDPOINT: process.env.ENDPOINT,
     CLUSTER: process.env.CLUSTER,
   },
-  webpack(config, { isServer }) {
+  webpack(config, { isServer, dev }) {
     config.experiments = { asyncWebAssembly: true, layers: true }
     // This is hack to fix the import of the wasm files https://github.com/vercel/next.js/issues/25852
-    if (isServer) {
-      config.output.webassemblyModuleFilename =
-        './../static/wasm/[modulehash].wasm'
-    } else {
-      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
+    if (!dev && isServer) {
+      config.output.webassemblyModuleFilename = 'chunks/[id].wasm'
+      config.plugins.push(new WasmChunksFixPlugin())
     }
     config.optimization.moduleIds = 'named'
     // End of hack
@@ -31,4 +29,23 @@ module.exports = {
     }
     return config
   },
+}
+
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap('WasmChunksFixPlugin', (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: 'WasmChunksFixPlugin' },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return
+            compilation.deleteAsset(pathname)
+
+            const name = pathname.split('/')[1]
+            const info = compilation.assetsInfo.get(pathname)
+            compilation.emitAsset(name, source, info)
+          })
+      )
+    })
+  }
 }
