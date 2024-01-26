222ae1e11b64f1cb61883ddbbfeb48d303a2b835
Guillermo Bescos <g.bescos@yahoo.com>
2024-01-26 15:08:29 +0000

    test vercel

--- frontend/pages/api/v1/snapshot.ts
+++ frontend/pages/api/v1/snapshot.ts
@@ -4,6 +4,20 @@ import { getAllStakeAccounts } from '../getAllStakingAccounts'
 import { BorshCoder, Idl } from '@coral-xyz/anchor'
 import IDL from '@pythnetwork/staking/target/idl/staking.json'
 import { PublicKey } from '@solana/web3.js'
+import { IdlCoder } from '@coral-xyz/anchor/dist/cjs/coder/borsh/idl'
+import { IdlTypeDef } from '@coral-xyz/anchor/dist/cjs/idl'
+
+const optionPositionType = {
+  name: 'OptionPosition',
+  type: {
+    kind: 'struct',
+    fields: [{ name: 'val', type: { option: { defined: 'Position' } } }],
+  },
+}
+const optionPositionLayout = IdlCoder.typeDefLayout(
+  optionPositionType as unknown as IdlTypeDef,
+  (IDL as Idl).types
+)
 
 const RPC_URL = process.env.BACKEND_ENDPOINT!
 
@@ -15,19 +29,32 @@ export default async function handlerSnapshot(
   await ZstdInit()
   const stakeAccounts = await getAllStakeAccounts(RPC_URL)
 
-  const owners: PublicKey[] = Object.keys(stakeAccounts).map((key) => {
+  const owners: number[] = Object.keys(stakeAccounts).map((key, index) => {
+    console.log(index, key.toString())
     return deserializePositionsAccount(stakeAccounts[key])
   })
 
   res.status(200).json(owners)
 }
 
-function deserializePositionsAccount(data: string): PublicKey {
+function deserializePositionsAccount(data: string): number {
   const buffer = Buffer.from(data, 'base64')
   const accountData = ZstdStream.decompress(new Uint8Array(buffer))
   const decoded = coder.accounts.decode(
     'PositionData',
     Buffer.from(accountData)
   )
-  return decoded.owner
+
+  const decodedPositions = []
+  for (let index = 0; index < 20; index++) {
+    let decodedPosition = optionPositionLayout.decode(
+      Buffer.from(decoded.positions[index])
+    ).val
+    decodedPositions.push(decodedPosition)
+    if (!decodedPosition) {
+      break
+    }
+  }
+
+  return decodedPositions.length
 }
