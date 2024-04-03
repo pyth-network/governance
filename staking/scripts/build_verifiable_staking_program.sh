2625ed91552dfda3cd62fecc122dfd9ce3631964
keyvan <keyvankhademi@gmail.com>
2024-04-03 12:54:23 -0700

    feat: add verifiable build to ci

--- staking/scripts/build_verifiable_staking_program.sh
+++ staking/scripts/build_verifiable_staking_program.sh
@@ -5,11 +5,34 @@ set -euo pipefail
 # Root of the repository
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-echo "Building the image for the staking program"
-docker build --platform linux/x86_64 -t staking-build -f "$REPO_ROOT"/staking/Dockerfile "$REPO_ROOT"/staking
+# Default value for TEST
+TEST=false
+
+# Parse command-line arguments
+# -t: build the staking program for tests
+while getopts "t" opt; do
+    case ${opt} in
+    t)
+        TEST=true
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" 1>&2
+        exit 1
+        ;;
+    esac
+done
+
+
+if [ "$TEST" = "true" ]; then
+    echo "Building the image for the staking program test"
+    docker build --platform linux/amd64 --build-arg TEST=true -t staking-build -f "$REPO_ROOT"/staking/Dockerfile "$REPO_ROOT"/staking
+else
+    echo "Building the image for the staking program production"
+    docker build --platform linux/amd64 -t staking-build -f "$REPO_ROOT"/staking/Dockerfile "$REPO_ROOT"/staking
+fi
 
 echo "Building the staking program"
-docker run --platform linux/x86_64 --rm -v "$REPO_ROOT"/staking/artifacts:/artifacts staking-build
+docker run --platform linux/amd64 --rm -v "$REPO_ROOT"/staking/artifacts:/artifacts staking-build
 
 echo "Successfully built the staking program."
 echo "The artifacts are available at $REPO_ROOT/staking/artifacts"
