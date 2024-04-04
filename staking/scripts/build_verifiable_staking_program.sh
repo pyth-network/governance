fabf0b3d9da883177b3da4ce061312d00fff7a84
keyvan <keyvankhademi@gmail.com>
2024-04-04 09:49:50 -0700

    test

--- staking/scripts/build_verifiable_staking_program.sh
+++ staking/scripts/build_verifiable_staking_program.sh
@@ -22,17 +22,21 @@ while getopts "t" opt; do
     esac
 done
 
-
 if [ "$TEST" = "true" ]; then
     echo "Building the image for the staking program test"
-    docker build --platform linux/amd64 --build-arg TEST=true -t staking-build -f "$REPO_ROOT"/staking/Dockerfile "$REPO_ROOT"/staking
+    docker build --platform linux/amd64 --build-arg TEST=true -t staking-build-test -f "$REPO_ROOT"/staking/Dockerfile "$REPO_ROOT"/staking
 else
     echo "Building the image for the staking program production"
     docker build --platform linux/amd64 -t staking-build -f "$REPO_ROOT"/staking/Dockerfile "$REPO_ROOT"/staking
 fi
 
-echo "Building the staking program"
-docker run --platform linux/amd64 --rm -v "$REPO_ROOT"/staking/target:/workspace/target staking-build
+if [ "$TEST" = "true" ]; then
+    echo "Building the staking program test"
+    docker run --platform linux/amd64 --rm -v "$REPO_ROOT"/staking/target:/workspace/target staking-build-test
+else
+    echo "Building the staking program"
+    docker run --platform linux/amd64 --rm -v "$REPO_ROOT"/staking/target:/workspace/target staking-build
+fi
 
 echo "Successfully built the staking program."
 echo "The artifacts are available at $REPO_ROOT/staking/target"
