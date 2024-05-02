d47665555c81c9185ced80712aa6e117d42a2b39
Connor Prussin <connor@prussin.net>
2024-05-02 10:26:25 -0700

    Add nix shell config

---
+++ shell.nix
@@ -0,0 +1,72 @@
+{
+  sources ? import ./sources.nix,
+  nixpkgs ? sources.nixpkgs,
+  niv ? sources.niv,
+  mkCli ? sources.mkCli,
+  rust-overlay ? sources.rust-overlay,
+}: let
+  niv-overlay = self: _: {
+    niv = self.symlinkJoin {
+      name = "niv";
+      paths = [niv];
+      buildInputs = [self.makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/niv \
+          --add-flags "--sources-file ${toString ./sources.json}"
+      '';
+    };
+  };
+
+  mkCli-overlay = import "${mkCli}/overlay.nix";
+
+  pkgs = import nixpkgs {
+    overlays = [
+      niv-overlay
+      mkCli-overlay
+      (import rust-overlay)
+    ];
+    config = {};
+  };
+
+  cli = pkgs.lib.mkCli "cli" {
+    _noAll = true;
+
+    install = "${pkgs.nodejs}/bin/npm ci";
+
+    start = "${pkgs.nodejs}/bin/npm run -w staking build && ${pkgs.nodejs}/bin/npm run -w frontend dev";
+
+    test = {
+      nix = {
+        lint = "${pkgs.statix}/bin/statix check --ignore node_modules .";
+        dead-code = "${pkgs.deadnix}/bin/deadnix --exclude ./node_modules .";
+        format = "${pkgs.alejandra}/bin/alejandra --exclude ./node_modules --check .";
+      };
+    };
+
+    fix = {
+      nix = {
+        lint = "${pkgs.statix}/bin/statix fix --ignore node_modules .";
+        dead-code = "${pkgs.deadnix}/bin/deadnix --exclude ./node_modules -e .";
+        format = "${pkgs.alejandra}/bin/alejandra --exclude ./node_modules .";
+      };
+    };
+  };
+in
+  pkgs.mkShell {
+    FORCE_COLOR = 1;
+    nativeBuildInputs = [pkgs.pkg-config];
+    buildInputs = [
+      cli
+      pkgs.git
+      pkgs.niv
+      pkgs.nodejs
+      pkgs.python3
+      pkgs.systemd
+      (
+        pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-std"];
+          targets = ["wasm32-unknown-unknown"];
+        }
+      )
+    ];
+  }
--- staking/scripts/build_wasm.sh
+++ staking/scripts/build_wasm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd programs/staking/
 wasm-pack build  -d ../../../wasm/bundle -- --features wasm
 wasm-pack build  -d ../../../wasm/node -t nodejs -- --features wasm
